b9d23dbe4dcb4afb135ee1e809b59d22b9d682da
Fred Sauer <fredsa@google.com>
2024-02-10 11:48:28 -0800

    list-models

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -30,51 +31,15 @@ func main() {
 	}
 	defer client.Close()
 
-	// Use PaLM 2 for Text (text-bison) model.
-	model := client.GenerativeModel("text-bison-001")
-
-	// Configure generation settings.
-	temperature := float32(0.7)
-	candidateCount := int32(4)
-	topK := int32(40)
-	topP := float32(0.95)
-	maxOutputTokens := int32(1024)
-
-	model.GenerationConfig = genai.GenerationConfig{
-		Temperature:     &temperature,
-		CandidateCount:  &candidateCount,
-		TopK:            &topK,
-		TopP:            &topP,
-		MaxOutputTokens: &maxOutputTokens,
-		StopSequences: []string{
-			"a",
-			"a\b",
-			"'a'",
-			`"a"`,
-		},
-	}
-
-	// Configure safety settings.
-	model.SafetySettings = []*genai.SafetySetting{
-		{Category: genai.HarmCategoryDerogatory, Threshold: genai.HarmBlockLowAndAbove},
-		{Category: genai.HarmCategoryToxicity, Threshold: genai.HarmBlockLowAndAbove},
-		{Category: genai.HarmCategoryViolence, Threshold: genai.HarmBlockMediumAndAbove},
-		{Category: genai.HarmCategorySexual, Threshold: genai.HarmBlockMediumAndAbove},
-		{Category: genai.HarmCategoryMedical, Threshold: genai.HarmBlockMediumAndAbove},
-		{Category: genai.HarmCategoryDangerous, Threshold: genai.HarmBlockMediumAndAbove},
-	}
-
-	mood := "exciting"
-	prompt := fmt.Sprintf("Write a creative%s children&#39;s bedtime &quot;story&quot;", mood)
-
-	// Call the Gemini AI API.
-	resp, err := model.GenerateContent(ctx, genai.Text(prompt))
-	if err != nil {
-		log.Fatalf("Error sending message: %v\n", err)
-	}
-
-	// Display the response.
-	for _, part := range resp.Candidates[0].Content.Parts {
-		fmt.Printf("%v\n", part)
+	it := client.ListModels(ctx)
+	for {
+		m, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error iterating through models: %v\n", err)
+		}
+		fmt.Printf("%v     %v, version %v, max %v tokens\n", m.Name, m.DisplayName, m.Version, m.InputTokenLimit)
 	}
 }
