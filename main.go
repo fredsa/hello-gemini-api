4e18fc1e890d0afded39d1761e9f066cd84a01ef
Fred Sauer <fredsa@google.com>
2024-02-10 11:12:58 -0800

    freeform_gemini_text_image.ts

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
 )
+
+const jpegUrlBird = "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRYL5x5KonnTYgeE-C-s09bBKuupEp0F0VsPYKNwpW6Xp-TFfoZ_iTporuLNOBWwm6HhOoek5cF"
+const jpegUrlCat = "https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcRbNuiexLb-Bsa2FR_HX5wRzIfI79zoNJk1F0kjbPvQ0O_MK3T-xobhQjN4fbYwDBa-RNdCx36t"
 
 func getApiKey() string {
 	envKey := "GEMINI_API_KEY"
@@ -30,26 +35,20 @@ func main() {
 	}
 	defer client.Close()
 
-	// Use Gemini Pro model.
-	model := client.GenerativeModel("gemini-pro")
+	// Use Gemini Pro Vision model.
+	model := client.GenerativeModel("gemini-pro-vision")
 
 	// Configure generation settings.
-	temperature := float32(0.9)
-	topK := int32(1)
+	temperature := float32(0.4)
+	topK := int32(32)
 	topP := float32(1.0)
-	maxOutputTokens := int32(2048)
+	maxOutputTokens := int32(4096)
 
 	model.GenerationConfig = genai.GenerationConfig{
 		Temperature:     &temperature,
 		TopK:            &topK,
 		TopP:            &topP,
 		MaxOutputTokens: &maxOutputTokens,
-		StopSequences: []string{
-			`a`,
-			`a\b`,
-			`'a'`,
-			`"a"`,
-		},
 	}
 
 	// Configure safety settings.
@@ -60,9 +59,16 @@ func main() {
 		{Category: genai.HarmCategoryDangerousContent, Threshold: genai.HarmBlockMediumAndAbove},
 	}
 
+	// Multi-part request.
+	parts := []genai.Part{
+		genai.Text("wut this?\n\n"),
+		genai.ImageData("jpeg", getJpegImageBytes(jpegUrlBird)),
+		genai.Text(" a birb\n\nand this: "),
+		genai.ImageData("jpeg", getJpegImageBytes(jpegUrlCat)),
+	}
+
 	// Call the Gemini AI API.
-	parts := genai.Text(`Write a creative\exciting children's bedtime "story"`)
-	resp, err := model.GenerateContent(ctx, parts)
+	resp, err := model.GenerateContent(ctx, parts...)
 	if err != nil {
 		log.Fatalf("Error sending message: %v\n", err)
 	}
@@ -71,4 +77,18 @@ func main() {
 	for _, part := range resp.Candidates[0].Content.Parts {
 		fmt.Printf("%v\n", part)
 	}
+}
+
+func getJpegImageBytes(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error fetching image %v: %v\n", url, err)
+	}
+	defer resp.Body.Close()
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading image bytes %v: %v\n", url, err)
+	}
+	return bytes
 }
